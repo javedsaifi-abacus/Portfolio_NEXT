d2a8f4c435ddd599e957847aedec50c214001f09
javed-tlp <saifijaved616@gmail.com>
2024-09-02 18:40:55 +0530

    "Added a new line after importing Navbar component in page.tsx"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Header from "../app/components/Header";
 import Section from "../app/Section/page";
 import ContactForm from "../app/components/ContactForm";
 import Navbar from "../app/components/Navbar";
+
 // import "../styles/globals.css"; // Import global styles
 
 export default function Home() {
