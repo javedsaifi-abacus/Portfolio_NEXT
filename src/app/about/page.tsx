7dd6dc5e0bf91f2dfd43faf25c9d05b758052696
javed-tlp <saifijaved616@gmail.com>
2024-09-02 13:02:21 +0530

    "Refactor globals.css and page.tsx to simplify styles and layout"

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+// app/about/page.tsx
+export default function About() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <p>I am a web developer with experience in Next.js, TypeScript, and modern web technologies.</p>
+      </section>
+    );
+  }
+  
