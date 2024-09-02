c5241b23d4717413fae2b728a480646bc4a7dc0d
javed-tlp <saifijaved616@gmail.com>
2024-09-02 18:22:19 +0530

    "Added React import and changed Section component declaration to use React.FC"

--- src/app/Section/page.tsx
+++ src/app/Section/page.tsx
@@ -1,16 +1,17 @@
 // /src/components/Section.tsx
+import React from 'react';
+
 interface SectionProps {
-    id: string;
-    title: string;
-    children: React.ReactNode;
-  }
-  
-  const Section = ({ id, title, children }: SectionProps) => (
-    <section id={id} className="section">
-      <h2>{title}</h2>
-      {children}
-    </section>
-  );
-  
-  export default Section;
-  
+  id: string;
+  title: string;
+  children: React.ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({ id, title, children }) => (
+  <section id={id} className="section">
+    <h2>{title}</h2>
+    {children}
+  </section>
+);
+
+export default Section;
