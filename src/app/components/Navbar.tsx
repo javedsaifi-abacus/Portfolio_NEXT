e6920a0512a81ea3369c782ba297c9a8a552abe5
javed-tlp <saifijaved616@gmail.com>
2024-09-10 22:38:19 +0530

    "Added empty line at end of navbar.module.css file"

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav className={styles.nav}>
@@ -27,37 +28,37 @@ const Navbar: React.FC = () => {
 
       {/* Navigation items */}
       <ul className={`${styles["nav-links"]} ${menuOpen ? styles["show"] : styles["hide"]}`}>
-        <li>
+        <li onClick={closeMenu}>
           <Link href="/skills">
             <FontAwesomeIcon icon={faStar} className={styles["nav-icon"]} />
             Skills
           </Link>
         </li>
-        <li>
+        <li onClick={closeMenu}>
           <Link href="/experience">
             <FontAwesomeIcon icon={faBriefcase} className={styles["nav-icon"]} />
             Experience
           </Link>
         </li>
-        <li>
+        <li onClick={closeMenu}>
           <Link href="/projects">
             <FontAwesomeIcon icon={faHome} className={styles["nav-icon"]} />
             Projects
           </Link>
         </li>
-        <li>
+        <li onClick={closeMenu}>
           <Link href="/education">
             <FontAwesomeIcon icon={faGraduationCap} className={styles["nav-icon"]} />
             Education
           </Link>
         </li>
-        <li>
+        <li onClick={closeMenu}>
           <Link href="/tools">
             <FontAwesomeIcon icon={faTools} className={styles["nav-icon"]} />
             Tools
           </Link>
         </li>
-        <li>
+        <li onClick={closeMenu}>
           <a href="/resume.pdf" download className={styles["cv-link"]}>
             <FontAwesomeIcon icon={faDownload} className={styles["nav-icon"]} />
             Download CV
