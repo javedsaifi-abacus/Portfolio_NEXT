e85355da3f96ede180c95636b2573d2f4697f01e
javed-tlp <saifijaved616@gmail.com>
2024-09-19 23:35:13 +0530

    "Updated @types/node and @types/react versions in package.json and package-lock.json, and modified Navbar.tsx component"

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: React.FC = () => {
         <li onClick={closeMenu}>
           <Link href="/skills">
             <FontAwesomeIcon icon={faStar} className={styles["nav-icon"]} />
-            Skills
+            jjjjjjj
           </Link>
         </li>
         <li onClick={closeMenu}>
