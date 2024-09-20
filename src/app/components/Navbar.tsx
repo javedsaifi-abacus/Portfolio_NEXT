4bb63792050a3cee2c95a1851c9976d8bd07e947
javed-tlp <saifijaved616@gmail.com>
2024-09-20 13:17:05 +0530

    "Removed 'Experience' menu item, reordered and readded 'Experience' and 'Education' menu items, and added new 'Tools' menu item to Navbar component."

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -35,27 +35,29 @@ const Navbar: React.FC = () => {
           </Link>
         </li>
         <li onClick={closeMenu}>
-          <Link href="/experience">
-            <FontAwesomeIcon icon={faBriefcase} className={styles["nav-icon"]} />
-            Experience
-          </Link>
-        </li>
-        <li onClick={closeMenu}>
           <Link href="/projects">
             <FontAwesomeIcon icon={faHome} className={styles["nav-icon"]} />
             Projects
           </Link>
         </li>
         <li onClick={closeMenu}>
-          <Link href="/education">
-            <FontAwesomeIcon icon={faGraduationCap} className={styles["nav-icon"]} />
-            Education
+          <Link href="/experience">
+            <FontAwesomeIcon icon={faBriefcase} className={styles["nav-icon"]} />
+            Experience
           </Link>
         </li>
+        
+        
         <li onClick={closeMenu}>
           <Link href="/tools">
             <FontAwesomeIcon icon={faTools} className={styles["nav-icon"]} />
             Tools
+          </Link>
+        </li>
+        <li onClick={closeMenu}>
+          <Link href="/education">
+            <FontAwesomeIcon icon={faGraduationCap} className={styles["nav-icon"]} />
+            Education
           </Link>
         </li>
         <li onClick={closeMenu}>
